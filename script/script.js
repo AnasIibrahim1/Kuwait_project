9e10bc346c7e56e17b0f4b8c42d1385d272105d5
AnasIibrahim1 <misi25_anas@yahoo.com>
2024-07-21 16:56:12 +0300

    upload

---
+++ script/script.js
@@ -0,0 +1,5 @@
+// script.js
+function toggleMenu() {
+    const dropdownMenu = document.querySelector('.dropdown-menu');
+    dropdownMenu.style.display = dropdownMenu.style.display === 'flex' ? 'none' : 'flex';
+  }
